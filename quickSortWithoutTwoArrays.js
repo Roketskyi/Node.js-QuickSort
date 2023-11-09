581f6bc5f0b9c502b9e252f29cca30c31b80c843
Roketskyi Roman <91413976+Roketskyi@users.noreply.github.com>
2023-11-09 12:35:11 +0200

    Update quickSortWithoutTwoArrays.js

--- quickSortWithoutTwoArrays.js
+++ quickSortWithoutTwoArrays.js
@@ -1,16 +1,18 @@
 const arr = [3, 7, 0, 1, 4, 2];
 
-function quickSort(arr, ascending = true, left = 0, right = arr.length - 1) {
+function quickSort(arr, ascending, left = 0, right = arr.length - 1) {
   if (left < right) {
     let i = left - 1;
 
     for (let j = left; j < right; j++) {
+      // Порівняння елементів з півотом залежно від напрямку сортування
       if ((ascending && arr[j] < arr[right]) || (!ascending && arr[j] > arr[right])) {
         i++;
         [arr[i], arr[j]] = [arr[j], arr[i]];
       }
     }
 
+    // Перестановка півоту на його правильне місце
     [arr[i + 1], arr[right]] = [arr[right], arr[i + 1]];
 
     const pivotIndex = i + 1;
@@ -22,5 +24,5 @@ function quickSort(arr, ascending = true, left = 0, right = arr.length - 1) {
   return arr;
 }
 
-console.log("Сортування за зростанням:", quickSort(arr.slice(), true));
-console.log("Сортування за спаданням:", quickSort(arr.slice(), false));
+console.log("Сортування за зростанням:", quickSort(arr, true));
+console.log("Сортування за спаданням:", quickSort(arr, false));
